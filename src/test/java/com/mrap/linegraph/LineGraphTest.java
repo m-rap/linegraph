04926248dfc58c63c8c451bb943da805296107fb
Rian Prakoso <mohammad.rian.ap@gmail.com>
2019-08-09 17:44:18 +0700

    samples and tests

---
+++ src/test/java/com/mrap/linegraph/LineGraphTest.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mrap.linegraph;
+
+import com.mrap.common.RandomDataGenerator;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.event.ActionEvent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+/**
+ *
+ * @author software
+ */
+public class LineGraphTest extends Application {
+    
+    LineGraph lineGraph, lineGraph2;
+    RandomDataGenerator.RandomGenericGenerator randomGenerator = new RandomDataGenerator.RandomGenericGenerator(10);
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        randomGenerator.listeners.add((RandomDataGenerator.RandomGeneratorListener<float[]>) (float[] result) -> {
+            lineGraph.addData(System.currentTimeMillis(), result[0], result[1], result[2]);
+        });
+        //lineGraph = new LineGraph(-0.1f, 0.1f, 1, 5, 100);
+        lineGraph = new LineGraph(-5f, 5f, 1, 5, 100);
+        lineGraph2 = new LineGraph(-400f, 400f, 50, 5, 100);
+        Button saveBtn = new Button("Save");
+        Button loadBtn = new Button("Load");
+        Button startBtn = new Button("Start");
+        Button startOnceBtn = new Button("Start Once");
+        Button stopBtn = new Button("Stop");
+        HBox hbox = new HBox(startBtn, startOnceBtn, stopBtn, saveBtn, loadBtn);
+        VBox box = new VBox(lineGraph, lineGraph2, hbox);
+        AnchorPane pane = new AnchorPane(box);
+        pane.setPrefWidth(600);
+        pane.setPrefHeight(400);
+        AnchorPane.setTopAnchor(box, 0.0);
+        AnchorPane.setRightAnchor(box, 0.0);
+        AnchorPane.setBottomAnchor(box, 0.0);
+        AnchorPane.setLeftAnchor(box, 0.0);
+        
+        saveBtn.setOnAction((ActionEvent event) -> {
+            SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy-HHmmss");
+            Date date;
+            if (lineGraph.getStartMs() != -1) {
+                date = new Date(lineGraph.getStartMs());
+            } else {
+                date = new Date();
+            }
+        });
+        
+        startBtn.setOnAction((ActionEvent event) -> {
+            try {
+                randomGenerator.start();
+            } catch (Exception ex) {
+                Logger.getLogger(LineGraphTest.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        });
+        
+        startOnceBtn.setOnAction((ActionEvent event) -> {
+            long start = System.currentTimeMillis();
+            int[] counter = new int[] {0};
+            randomGenerator.nextMsRandoms(900000, (Object res) -> {
+                long ts = (long)counter[0]*(randomGenerator.delay+1)+start;
+                float[] result = (float[])res;
+                //String[] str = new String[6];
+                //for (int i = 0; i < 3; i++)
+                //    str[i] = result[i] + "";
+                //System.out.println(ts + " " + String.join(" ", str));
+                lineGraph.addData(ts, result[0], result[1], result[2]);
+                counter[0]++;
+            });
+        });
+        
+        stopBtn.setOnAction((ActionEvent event) -> {
+            randomGenerator.stop();
+        });
+        
+        primaryStage.setScene(new Scene(pane));
+        primaryStage.setOnCloseRequest((WindowEvent event) -> {
+            randomGenerator.stop();
+            lineGraph.runnable.stop();
+        });
+        primaryStage.show();
+        lineGraph.runnable.start();
+        lineGraph2.runnable.start();
+        //testRecording();
+    }
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/test/java/com/mrap/linegraph/MainWindowController.java
@@ -0,0 +1,142 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mrap.linegraph;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.animation.AnimationTimer;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.ScrollEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+
+/**
+ * FXML Controller class
+ *
+ * @author Rian
+ */
+public class MainWindowController implements Initializable {
+
+    final static long ANIMATION_TIMEOUT = 1000;
+    final static int FPS = 30;
+    
+    @FXML
+    AnchorPane mixerPane;
+    @FXML
+    Canvas canvas;
+    
+    GraphicsContext gc;
+    double pixPerMs;
+    int unitTick = 1000;
+    
+    long startTs;
+    
+    long animationTime = ANIMATION_TIMEOUT;
+    long animationStart;
+    long delay;
+    
+    private double minPixPerMs;
+    double mouseX = 0, mouseY = 0;
+    AnimationTimer animTimer = new AnimationTimer() {
+        @Override
+        public void handle(long now) {
+            if (animationTime >= ANIMATION_TIMEOUT) {
+                animationTime = ANIMATION_TIMEOUT;
+                animTimer.stop();
+                return;
+            }
+            draw();
+            animationTime = System.currentTimeMillis() - animationStart;
+        }
+    };
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        startTs = 0; //System.currentTimeMillis();
+        
+        gc = canvas.getGraphicsContext2D();
+        canvas.setOnScroll((ScrollEvent event) -> {
+            double x = event.getX();
+            long midTs = (long)(x / pixPerMs) + startTs;
+            pixPerMs += event.getDeltaY() * 0.0001;
+            if (pixPerMs <= minPixPerMs)
+                pixPerMs = minPixPerMs;
+            startTs = midTs - (long)(x / pixPerMs);
+            startAnimation();
+        });
+        canvas.setOnMouseMoved((MouseEvent event) -> {
+            mouseX = event.getX();
+            mouseY = event.getY();
+            startAnimation();
+        });
+        delay = 1000 / FPS;
+        minPixPerMs = canvas.getHeight() / (60 * 1000);
+        pixPerMs = minPixPerMs;
+        startAnimation();
+    }
+    
+    void startAnimation() {
+        animationTime = 0;
+        animationStart = System.currentTimeMillis();
+        animTimer.start();
+    }
+    
+    void draw() {
+        double width = canvas.getWidth(),
+               height = canvas.getHeight();
+        gc.clearRect(0, 0, width, height);
+        double unitTickLength = unitTick * pixPerMs;
+        
+        int upperUnitTick, lowerUnitTick;
+        int digitNum = (int)Math.floor(Math.log10(unitTick));
+        int base = (int)Math.pow(10, digitNum);
+        if (unitTick == base) {
+            upperUnitTick = (int)(5 * Math.pow(10, digitNum));
+            lowerUnitTick = unitTick / 2;
+        } else {
+            upperUnitTick = unitTick * 2;
+            lowerUnitTick = (int)Math.pow(10, digitNum);
+        }
+        
+        if (unitTickLength < 50.0) {
+            unitTick = upperUnitTick;
+        } else if (unitTickLength >= 100.0) {
+            if (lowerUnitTick * pixPerMs >= 50.0) {
+                unitTick = lowerUnitTick;
+            }
+        }
+        
+        gc.setStroke(new Color(0, 0, 0, 1));
+        long i = startTs / unitTick;
+        long mod = startTs % unitTick;
+        if (mod > unitTick / 2) {
+            i++;
+        }
+        i *= unitTick;
+        
+        double fontSize = gc.getFont().getSize();
+        for (double x =  pixPerMs * (i - startTs); x < width; i += unitTick) {
+            gc.beginPath();
+            x =  pixPerMs * (i - startTs);
+            gc.moveTo(x, 0);
+            gc.lineTo(x, height);
+            gc.stroke();
+            gc.closePath();
+            
+            gc.strokeText("" + i, x, fontSize);
+        }
+        gc.strokeText(String.format("%.4f\n%d\n%d\n%.4f", mouseX, 
+                (long)((mouseX / pixPerMs) + startTs), startTs, pixPerMs),
+                5, height - 50);
+    }
+}
